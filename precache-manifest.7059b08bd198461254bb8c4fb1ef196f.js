29c527b7dc81f84e817861a30046048ed1b71137
KalinkinaD <dkalinina8@gmail.com>
2020-08-01 00:35:14 +0300

    Updates

---
+++ precache-manifest.7059b08bd198461254bb8c4fb1ef196f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6c8f868b08dc9f321cca0557d7265ed",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "102fa19178edb8a13b15",
+    "url": "/goit-react-hw-02-feedback/static/css/main.b580af2f.chunk.css"
+  },
+  {
+    "revision": "316a199b096426a28a74",
+    "url": "/goit-react-hw-02-feedback/static/js/2.c5e1403d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.c5e1403d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "102fa19178edb8a13b15",
+    "url": "/goit-react-hw-02-feedback/static/js/main.717f7270.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
